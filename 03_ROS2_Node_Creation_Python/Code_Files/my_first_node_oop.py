03a62f49e1630223f3ea8b4c4a5f27a42256f9e1
Arash Barabadi <54539090+Arash-Barabadi@users.noreply.github.com>
2024-04-07 00:47:03 +0200

    Update my_first_node_oop.py

--- 03_ROS2_Node_Creation_Python/Code_Files/my_first_node_oop.py
+++ 03_ROS2_Node_Creation_Python/Code_Files/my_first_node_oop.py
@@ -4,13 +4,21 @@ from rclpy.node import Node
 
 # Generate a class from the above imported Node object.
 class MyNode(Node):
-    # then we wright the constructure of the class
+    # then we wright the constructor of the class
     def __init__(self): 
         # the below __init__ funtion called from the Node which is imported. 
         super().__init__("py_test")
+        self.counter_ = 0
         self.get_logger().info("Hello ROS2")
+        self.create_timer(1, self.timer_callback)
+        
 
 
+# Timer will allow you to call the function at a given rate. 
+#For example, you want to call a function at 10Hz then timer comes into play.
+    def timer_callback(self):
+        self.counter_ += 1
+        self.get_logger().info("Hello " + str(self.counter_))
 
 def main(args=None):
 
@@ -21,5 +29,3 @@ def main(args=None):
 
 if __name__ == "__main__" : 
     main()
- 
-    
