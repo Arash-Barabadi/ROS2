2239ac85accefc68f048d962dfec7be15ebfce16
Arash Barabadi <54539090+Arash-Barabadi@users.noreply.github.com>
2024-11-24 21:13:26 +0100

    Update add_two_ints_client_no_oop.py

--- 05_ROS2_Communication_Mechanims/Services/Client_Python/add_two_ints_client_no_oop.py
+++ 05_ROS2_Communication_Mechanims/Services/Client_Python/add_two_ints_client_no_oop.py
@@ -57,18 +57,3 @@ def main(args=None):
 if __name__ == "__main__":
     main()
     
-    
-####---------------------------------------------------------------------------------------------------------------
-
-#How It Works
-
-#    The node add_two_ints_no_oop is started.
-#    It creates a client for the add_two_ints service and waits for the corresponding server to be available.
-#    When the server is ready, the client sends a request containing two integers (3 and 8).
-#    The client waits for the server's response.
-#    Once the response is received:
-#        If successful, logs the result ("3 + 8 = 11").
-#        If an error occurs, logs an error message.
-#    Shuts down the node.
-
-###----------------------------------------------------------------------------------------------------------------
